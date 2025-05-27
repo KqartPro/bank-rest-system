9cce63f76c39fba1c3048c62a8a6835af2a3d25b
Kqart <kqartpro@gmail.com>
2025-05-27 14:06:44 +0500

    initial commit

---
+++ src/test/java/kz/pryakhin/bankrest/BankRestApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.pryakhin.bankrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
