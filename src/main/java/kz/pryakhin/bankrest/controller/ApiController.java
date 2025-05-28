6a3aea2642f4b6b0548533482a0301b348816d28
Kqart <kqartpro@gmail.com>
2025-05-28 17:33:58 +0500

    api versioning and swagger configuration

---
+++ src/main/java/kz/pryakhin/bankrest/controller/ApiController.java
@@ -0,0 +1,7 @@
+package kz.pryakhin.bankrest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("${api.base-path}")
+public abstract class ApiController {
+}
